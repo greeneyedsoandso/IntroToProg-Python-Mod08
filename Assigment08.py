f54aae7a4159bda65aba14334907276a20ac323f
greeneyedsoandso <27257805+greeneyedsoandso@users.noreply.github.com>
2020-03-11 18:53:11 -0700

    Lifted code from Assignment06 and got it to run

--- Assigment08.py
+++ Assigment08.py
@@ -6,6 +6,7 @@
 # RRoot,1.1.2030,Created started script
 # RRoot,1.1.2030,Added pseudo-code to start assignment 8
 # JDSmith,3.10.20,Renamed and started file modifications
+# JDSmith,3.10.20,Lifted code from Assignment06, started refactoring
 # ------------------------------------------------------------------------ #
 # TODO: Look at previous assignments for the base functionality, focus on new concepts
 # Data -------------------------------------------------------------------- #
@@ -26,7 +27,14 @@ class Product:
     pass
     # TODO: Add Code to the Product class
 # Data -------------------------------------------------------------------- #
-
+# Declare variables and constants
+objFile = None   # An object that represents a file
+dicRow = {}  # A row of data separated into elements of a dictionary {Task,Priority}
+lstTable = []  # A list that acts as a 'table' of rows
+strChoice = ""  # Captures the user option selection
+strTask = ""  # Captures the user task data
+strPriority = ""  # Captures the user priority data
+strStatus = ""  # Captures the status of an processing functions
 # Processing  ------------------------------------------------------------- #
 class FileProcessor:
     """Processes data to and from a file and a list of product objects:
@@ -42,7 +50,49 @@ class FileProcessor:
     """
     pass
     # TODO: Add Code to process data from a file
+    @staticmethod
+    def read_data_from_file(file_name, list_of_rows):
+        """ Reads data from a file into a list of dictionary rows
+
+        :param file_name: (string) with name of file:
+        :param list_of_rows: (list) you want filled with file data:
+        :return: (list) of dictionary rows:
+        """
+        list_of_rows.clear()  # clear current data
+        file = open(file_name, "r")
+        for line in file:
+            task, priority = line.split(",")
+            row = {"Task": task.strip(), "Priority": priority.strip()}
+            list_of_rows.append(row)
+        file.close()
+
+    @staticmethod  # 5
+    def add_data_to_list(task, priority, list_of_rows):
+        """
+        Appends rows to the list of dictionary rows
+        :param task: value for key Task:
+        :param priority: value for key Priority:
+        :param list_of_rows: list where data should go:
+        :return: rows in list, success message:
+        """
+        # TODO: Add Code Here!
+        list_of_rows.append({'Task': task, 'Priority': priority})
+        return list_of_rows, 'Success'
+
     # TODO: Add Code to process data to a file
+    @staticmethod
+    def write_data_to_file(file_name, list_of_rows):
+        """
+        Writes data in memory into a file
+        :param file_name: the file name as a string:
+        :param list_of_rows: list of dictionary rows:
+        :return: current contents:
+        """
+        file = open(file_name, 'w')
+        for row in lstTable:
+            file.write(str(row['Task'] + ',' + str(row['Priority'] + '\n')))
+        file.close()
+        return list_of_rows, 'Success'
 
 # Processing  ------------------------------------------------------------- #
 
@@ -51,9 +101,73 @@ class IO:
     # TODO: Add docstring
     pass
     # TODO: Add code to show menu to user
+    @staticmethod  # 2 this shows the menu
+    def print_menu_tasks():
+        """  Display a menu of choices to the user
+
+        :return: nothing
+        """
+        print('''
+            Menu of Options
+            1) Add a new Task
+            2) Remove an existing Task
+            3) Save Data to File        
+            4) Reload Data from File
+            5) Exit Program
+            ''')
+        print()  # Add an extra line for looks
+
     # TODO: Add code to get user's choice
+    @staticmethod # 3 this lets you choose a menu item
+    def input_menu_choice():
+        """ Gets the menu choice from a user
+
+        :return: string
+        """
+        choice = str(input("Which option would you like to perform? [1 to 5] - ")).strip()
+        print()  # Add an extra line for looks
+        return choice
+    @staticmethod
+    def input_yes_no_choice(message):
+        """ Gets a yes or no choice from the user
+
+        :return: string
+        """
+        return str(input(message)).strip().lower()
+
+    @staticmethod  # 6
+    def input_press_to_continue(optional_message=''):
+        """ Pause program and show a message before continuing
+
+        :param optional_message:  An optional message you want to display
+        :return: nothing
+        """
+        print(optional_message)
+        input('Press the [Enter] key to continue.')
     # TODO: Add code to show the current data from the file to user
+    @staticmethod  # 1 this is the stuff in the dictionary rows that are loaded in memory
+    def print_current_tasks_in_list(list_of_rows):
+        """ Shows the current Tasks in the list of dictionaries rows
+
+        :param list_of_rows: (list) of rows you want to display
+        :return: nothing
+        """
+        print("******* The current Tasks ToDo are: *******")
+        for row in list_of_rows:
+            print(row["Task"] + " (" + row["Priority"] + ")")
+        print("*******************************************")
+        print()  # Add an extra line for looks
+
     # TODO: Add code to get product data from user
+    @staticmethod  # 4
+    def input_new_task_and_priority():
+        """
+        Gets new Task and Priority from user
+        :return: task and priority strings from user input
+        """
+        task = input('Task: ')
+        priority = input('Priority: ')
+        return task, priority
 # Presentation (Input/Output)  -------------------------------------------- #
 
 # Main Body of Script  ---------------------------------------------------- #
@@ -66,4 +180,38 @@ class IO:
     # let user save current data to file and exit program
 
 # Main Body of Script  ---------------------------------------------------- #
+# Load data from file into a list of product objects when script starts
+FileProcessor.read_data_from_file(strFileName, lstTable)  # read file data
 
+# Show user a menu of options
+while (True):
+    # Step 3 Show current data
+    IO.print_current_tasks_in_list(lstTable)  # Show current data in the list/table
+    IO.print_menu_tasks()  # Shows menu
+    strChoice = IO.input_menu_choice()  # Get menu option
+
+    # Step 4 - Process user's menu choice
+    if strChoice.strip() == '1':  # Add a new Task
+        strTask, strPriority = IO.input_new_task_and_priority()
+        FileProcessor.add_data_to_list(strTask, strPriority, lstTable)
+        IO.input_press_to_continue(strStatus)
+        continue  # to show the menu
+
+    elif strChoice == '2':  # Remove an existing Task
+        strTask = IO.input_task_to_remove()
+        FileProcessor.remove_data_from_list(strTask, lstTable)
+        IO.input_press_to_continue(strStatus)
+        continue  # to show the menu
+
+    elif strChoice == '3':  # Save Data to File
+        strChoice = IO.input_yes_no_choice("Save this data to file? (y/n) - ")
+        if strChoice.lower() == "y":
+            FileProcessor.write_data_to_file('products.txt', lstTable)
+            IO.input_press_to_continue(strStatus)
+        else:
+            IO.input_press_to_continue("Save Cancelled!")
+        continue  # to show the menu
+
+    elif strChoice == '5':  # Exit Program
+        print("Goodbye!")
+        break  # and Exit
